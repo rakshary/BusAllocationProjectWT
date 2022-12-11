9cc962e252a311eff1eb4d46f4ac6ee7be9c36ec
SowmyaSreeT <92526615+SowmyaSreeT@users.noreply.github.com>
2022-12-11 19:34:36 +0530

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,25 @@
+<html>
+<body>
+<?php
+$con = mysql_connect("localhost","root","Sowmyasree@2003");
+if (!$con)
+{
+die('Could not connect: ' . mysql_error());
+}
+mysql_select_db("bookings");
+$sql="INSERT INTO bus (bid,bname,source,dest,agency)
+ VALUES
+('$_POST[bid]','$_POST[bname]','$_POST[source]','$_POST[dest]','$_POST[agency]')";
+if (!mysql_query($sql,$con))
+{
+die('Error: ' . mysql_error());
+}
+echo "1 record added";
+mysql_close($con)
+?>
+<form action="showbookings.php" method="post">
+Name: <input type="text" name="Title" />
+<input type="submit" />
+</form>
+</body>
+</html> 
